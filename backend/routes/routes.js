bdf09982a788b338a0caf6c8d16a2ab64c67f4b8
willk0814 <wkoenig0814@gmail.com>
2023-05-05 14:50:58 -0400

    added front end directory

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,17 +2,23 @@ const express = require("express");
 const router = express.Router();
 const Model = require("../models/user_model");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 module.exports = router; // export the router to be used elsewhere
 
 // POST Method
 router.post("/register", async (req, res) => {
   console.log("Register request recieved");
+  const hashedPaswword = await bcrypt.hash(req.body.pass, 10);
   const data = new Model({
     user: req.body.user,
-    pass: req.body.pass,
+    pass: hashedPaswword,
     age: req.body.age,
   });
-
+  console.log(
+    "Registering new user with user, hashedPass: ",
+    data.user,
+    data.pass
+  );
   try {
     const dataToSave = await data.save();
 
@@ -31,11 +37,14 @@ router.post("/login", async (req, res) => {
   console.log("Login request recieved");
   const { user, pass } = req.body;
   try {
-    const data = await Model.findOne({ user, pass });
+    const data = await Model.findOne({ user });
     if (data) {
-      // generate a jwt to send to front end
-      const token = jwt.sign({ user }, "secret", { expiresIn: "1h" });
-      res.status(200).json({ user, token, authStatus: true });
+      const isMatch = await bcrypt.compare(pass, data.pass);
+      if (isMatch) {
+        // generate a jwt to send to front end
+        const token = jwt.sign({ user }, "secret", { expiresIn: "1h" });
+        res.status(200).json({ user, token, authStatus: true });
+      }
     } else {
       res
         .status(401)
